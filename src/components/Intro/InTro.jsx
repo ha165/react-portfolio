d217c5e794e6dc4b94b26d3d2d4ae66af4afc6b5
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-05-30 15:33:27 +0300

    create Intro section

---
+++ src/components/Intro/InTro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const InTro = () => {
+    return (
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-name">
+                    <span> Hey! I Am</span>
+                    <span>Lumumba Harmony</span>
+                    <span>Frontend And Backend Developer with better expirience in web designinng and development,producing a quality work</span>
+                </div>
+            </div>
+            <div className="i-right"></div>
+        </div>
+    )
+}
+
+export default InTro
