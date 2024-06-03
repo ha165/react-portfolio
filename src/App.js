f56812eaaeb3a78ec3cb3b9a8257105a22b27745
Harmony Lumumba Muyagane <66202605+ha165@users.noreply.github.com>
2024-06-03 13:06:30 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/Navbar/NavBar";
 import InTro from "./components/Intro/InTro";
+import Features from "./components/Features/Features";
 import './App.css';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <InTro />
+      <Features />
     </div>
   );
 }
---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Features.css';
+import HeartEmoji from '../../img/heartemoji.png';
+import Glasses from '../../img/glasses.png';
+import Humble from '../../img/humble.png';
+const Features = () => {
+    return (
+        <div className="services">
+            {/* Left Side*/}
+            <div className="awesome">
+                <span>My Awesome</span>
+                <span>services</span>
+                <spane>
+                    this are the services i offer and also the works
+                    <br />
+                    i have done in the past
+                </spane>
+                <div className="button s-button">Download CV</div>
+                <div className="blur"></div>
+            </div>
+            {/*ight side*/}
+            <div className="cards">
+               this is the right
+            </div>
+        </div>
+    )
+}
+
+export default Features
