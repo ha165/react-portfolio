8456d08894378363738d0df046c657b396f859f1
Harmony Lumumba Muyagane <66202605+ha165@users.noreply.github.com>
2024-06-04 16:35:42 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import NavBar from "./components/Navbar/NavBar";
 import InTro from "./components/Intro/InTro";
 import Tasks from "./components/Tasks/Tasks";
+import Experience from "./components/Experience/Experience";
+import Works from "./components/Works/Works";
 import './App.css';
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <NavBar />
       <InTro />
       <Tasks />
+      <Experience />
+      <Works />
     </div>
   );
 }
