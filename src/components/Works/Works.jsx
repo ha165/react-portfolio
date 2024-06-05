716c414c5f9f9b2738688d0daf9a2eaf970d4819
Harmony Lumumba Muyagane <66202605+ha165@users.noreply.github.com>
2024-06-05 10:45:23 +0300

    Update Works.jsx

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div className="works">
+      <div className="awesome">
+        <span>Works For all these</span>
+        <span>Brands & Client</span>
+        <spane>
+          Lorem ispum is simpley dummy text of printing of printing Lorem
+          <br />
+          ispum is simpley dummy text of printing
+          <br />
+          ispum is simpley dummy text of printing text of printing
+          <br />
+          Lorem ispum is simpley dummy text
+        </spane>
+        <button className="button s-button">Hire Me</button>
+        <div className="blur s-blur" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+    </div>
   )
 }
 
