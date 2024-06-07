238046785f42f5485662bfe8d2f3dbc8b6b2964f
ha165 <lumumbaharmony@gmail.com>
2024-06-07 13:16:53 +0300

    updates

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useContext, useRef, useState } from "react";
 import './Contact.css';
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+    const form = useRef();
+    const [done, setDone] = useState(false)
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm(
+          "service_z0eb7ym",
+          "template_2kxvsg8",
+          form.current,
+          "dC_PDlH8Z7a6Ycaeb"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setDone(true);
+            form.reset();
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    };
     return (
         <div className="contact-form">
             <div className="w-left">
@@ -11,7 +35,7 @@ const Contact = () => {
                 </div>
             </div>
             <div className="c-right">
-                <form action="">
+                <form ref={form} action="" onSubmit={sendEmail}>
                     <input type="text" name='user_name' className='user' placeholder='Name' />
                     <input type="email" name="user_email" className="user" placeholder="Email" />
                     <textarea name="message" className="user" placeholder="Message" />
