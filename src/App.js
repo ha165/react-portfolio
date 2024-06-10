b78c7e36fe058c8116ea30f83007953ba2b651b2
ha165 <lumumbaharmony@gmail.com>
2024-06-10 13:01:14 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import './App.css';
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Works />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
