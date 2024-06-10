dfaa3fcdff2606b0972f52a4cb2c049c6f93c133
ha165 <lumumbaharmony@gmail.com>
2024-06-10 16:18:52 +0300

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Portfolio.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Sidebar from "../../img/sidebar.png";
 import Ecommerce from "../../img/ecommerce.png";
 import HOC from "../../img/hoc.png";
 import MusicApp from "../../img/musicapp.png";
+import { themeContext } from "../../Context";
 import 'swiper/css';
 const Portfolio = () => {
+    const theme = useContext(themeContext);
+    const darkMode = theme.state.darkMode;
     return (
         <div className="portfolio">
             {/*Heading */}
-            <span>Recent Projects</span>
+            <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
             <span>Portfolio</span>
 
             {/*Slider*/}
