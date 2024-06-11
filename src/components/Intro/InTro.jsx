e198f31192d6806e853e029397755e1e57d73650
ha165 <lumumbaharmony@gmail.com>
2024-06-11 10:37:21 +0300

    changing node version

--- src/components/Intro/InTro.jsx
+++ src/components/Intro/InTro.jsx
@@ -5,23 +5,26 @@ import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
 import Vector1 from '../../img/Vector1.png';
 import Vector2 from '../../img/Vector2.png';
-import Boy from '../../img/boy.png';
 import me from '../../img/me.png';
 import Thumbup from '../../img/thumbup.png';
 import Crown from '../../img/crown.png';
 import glassesimoji from '../../img/glassesimoji.png';
 import { themeContext } from "../../Context";
+import { motion } from 'framer-motion';
 import './Intro.css';
+
 const InTro = () => {
+    const transition = { duration: 2, type: 'spring' };
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
+
     return (
         <div className="intro">
             <div className="i-left">
                 <div className="i-name">
                     <span style={{ color: darkMode ? "white" : "" }}> Hey! I Am</span>
                     <span>Lumumba Harmony</span>
-                    <span>Frontend And Backend Developer with better expirience in web designinng and development,producing a quality work</span>
+                    <span>Frontend And Backend Developer with better experience in web designing and development, producing a quality work</span>
                 </div>
                 <button className="button i-button">Hire me</button>
                 <div className="i-icons">
@@ -34,16 +37,21 @@ const InTro = () => {
                 <img src={Vector1} alt="" />
                 <img src={Vector2} alt="" />
                 <img src={me} alt="" />
-                <img src={glassesimoji} alt="" />
+                <motion.img
+                    initial={{ left: '-36%' }}
+                    whileInView={{ left: '-24%' }}
+                    transition={transition}
+                    src={glassesimoji}
+                    alt=""
+                />
                 <div style={{ top: '-4%', left: '68%' }}>
                     <FloatingDiv image={Crown} txt1='Web' txt2='Developer' />
                 </div>
                 <div style={{ top: '18rem', left: '0rem' }}>
                     <FloatingDiv image={Thumbup} txt1='Best Design' txt2='Award' />
                 </div>
-                {/* lur ddivs */}
-                <div className='blur' style={{ background: "rgb(263 210 255)" }}>
-                </div>
+                {/* blur divs */}
+                <div className='blur' style={{ background: "rgb(263 210 255)" }}></div>
                 <div className="blur" style={{
                     background: '#C1F5FF',
                     top: '17rem',
@@ -52,9 +60,8 @@ const InTro = () => {
                     left: '-9rem'
                 }}></div>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default InTro
+export default InTro;
