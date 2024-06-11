caff1b15b149a3937acf78ab5513fecbe6bcebbf
ha165 <lumumbaharmony@gmail.com>
2024-06-11 11:27:37 +0300

    Update Works.jsx

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import './Works.css';
 import Upwork from "../../img/Upwork.png";
 import Fiverr from "../../img/fiverr.png";
@@ -6,6 +6,7 @@ import Amazon from "../../img/amazon.png";
 import Shopify from "../../img/Shopify.png";
 import Facebook from "../../img/Facebook.png";
 import { themeContext } from "../../Context";
+import { motion } from 'framer-motion';
 const Works = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -29,7 +30,12 @@ const Works = () => {
       </div>
       {/* Right side */}
       <div className="w-right">
-        <div className="w-mainCircle">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle">
           <div className="w-secCircle">
             <img src={Upwork} alt="" />
           </div>
@@ -45,12 +51,12 @@ const Works = () => {
           <div className="w-secCircle">
             <img src={Facebook} alt="" />
           </div>
-        </div>
+        </motion.div>
         {/*Bacground Circles */}
         <div className="w-backCircle blueCircle"></div>
         <div className="w-backCircle yellowCircle"></div>
       </div>
-    </div>
+    </div >
   )
 }
 
