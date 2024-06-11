a4e5cfe9cdd06ad6c20bbee4264bf81afac87bd7
ha165 <lumumbaharmony@gmail.com>
2024-06-11 11:28:59 +0300

    Update NavBar.jsx

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import Toggle from '../Toggle/Toggle'
+import { Link; } from 'react-scroll'
 const NavBar = () => {
   return (
     <div className="n-wrapper"> 
